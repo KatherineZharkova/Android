96963f7433e93c8c1278567e2876539676e2e6ad
Cocovella <katherine.zharkova@gmail.com>
2019-11-05 03:33:21 +0300

    Lesson#2 1. В погодном приложении создайте еще один экран — экран для выбора города. На экране выбора города пользователь должен выбрать интересующий его город и по нажатию на кнопку переходить на уже сделанный вами экран отображения температуры. Как будут связаны и как будут взаимодействовать экраны выбора города и информации о температуре, мы узнаем из следующих занятий. Пока достаточно нарисовать экраны. 2. Экран выбора города может включать TextView для ввода города, список наиболее популярных городов и CheckBox для дополнительной информации: скорость ветра и давление. 3.* Покажите этот экран вместо предыдущего. 4.* Создайте еще один экран — экран настроек приложения. Добавьте на этот экран настройку смены темы на светлую и темную соответственно. Добавьте настройки по вашему выбору.

--- app/src/main/java/ru/cocovella/WeatherApp/MainActivity.java
+++ app/src/main/java/ru/cocovella/WeatherApp/MainActivity.java
@@ -1,14 +1,61 @@
 package ru.cocovella.WeatherApp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView cityName;
+    String city = "Moscow";
+    Button chooseCityButton;
+    ImageButton settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        onChooseCityButtonClick();
+        onSettingsButtonClick();
     }
+
+
+    private void initViews() {
+        cityName = findViewById(R.id.cityName);
+        cityName.setText(city);
+
+        chooseCityButton = findViewById(R.id.chooseCityButton);
+        settingsButton = findViewById(R.id.settingsButton);
+    }
+
+    private void onChooseCityButtonClick() {
+        chooseCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ChooseCityActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void onSettingsButtonClick() {
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 }
