0ca587fb78af2e1db35a2cbac00401f864eefd23
Katherine <46581738+KatherineZharkova@users.noreply.github.com>
2019-11-07 23:33:09 +0300

    hw#2
    Lesson#2
    1. В погодном приложении создайте экран для выбора города.
    Пользователь должен выбрать интересующий его город и по нажатию на кнопку переходить на экран отображения температуры.
    2. Экран выбора города включает TextView для ввода города, список наиболее популярных городов и CheckBox для дополнительной информации: скорость ветра и давление.
    3.* Покажите этот экран вместо предыдущего.
    4.* Создайте экран настроек приложения. Добавьте туда настройку смены темы на светлую и темную соответственно. Добавьте настройки по вашему выбору.
    
    * Updates:
    1. Город и настройки доп.инфо прогноза сохраняются;
    2. Чекбокс рабочий (в MainActivity видно только выбранную инфу);
    3. Свитч тем тоже рабочий;
    4. Добавлен фон, иконки приложения и кнопок;
    5. Изменены кнопки в настройках на Apply и перемещены вниз.
    6. Список городов изменен на RadioGroup;

--- app/src/main/java/ru/cocovella/WeatherApp/MainActivity.java
+++ app/src/main/java/ru/cocovella/WeatherApp/MainActivity.java
@@ -1,14 +1,106 @@
 package ru.cocovella.WeatherApp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView cityName;
+    Button chooseCityButton;
+    ImageButton settingsButton;
+    TextView humidityTV;
+    TextView humidityInfo;
+    TextView windTV;
+    TextView windInfo;
+    TextView barometerTV;
+    TextView barometerInfo;
+    SharedPreferences settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        settings = getSharedPreferences("settings", Context.MODE_PRIVATE);
+        setTheme(settings.getInt("THEME", R.style.AppTheme));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        onChooseCityButtonClick();
+        onSettingsButtonClick();
     }
+
+    private void initViews() {
+        cityName = findViewById(R.id.cityName);
+        cityName.setText(getCityName());
+
+        chooseCityButton = findViewById(R.id.chooseCityButton);
+        settingsButton = findViewById(R.id.settingsButton);
+
+        humidityTV = findViewById(R.id.humidityTV);
+        humidityInfo = findViewById(R.id.humidityInfo);
+        setExtraInfo("humidity", humidityTV, humidityInfo);
+
+        windTV = findViewById(R.id.windTV);
+        windInfo = findViewById(R.id.windInfo);
+        setExtraInfo("wind", windTV, windInfo);
+
+        barometerTV = findViewById(R.id.barometerTV);
+        barometerInfo = findViewById(R.id.barometerInfo);
+        setExtraInfo("barometer", barometerTV, barometerInfo);
+    }
+
+    private void onChooseCityButtonClick() {
+        chooseCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), ChooseCityActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onSettingsButtonClick() {
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private String getCityName() {
+        String cityFromIntent = getIntent().getStringExtra("city");
+
+        if (cityFromIntent != null) {
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("city", cityFromIntent);
+            editor.putBoolean("humidity", getIntent().getBooleanExtra("humidity", false));
+            editor.putBoolean("wind", getIntent().getBooleanExtra("wind", false));
+            editor.putBoolean("barometer", getIntent().getBooleanExtra("barometer", false));
+            editor.putInt("radioCity", getIntent().getIntExtra("radioCity", -1));
+            editor.apply();
+            return cityFromIntent;
+        } else {
+            return settings.getString("city", "Atlantis");
+        }
+    }
+
+    private void setExtraInfo(String key, TextView keyTV, TextView keyInfo) {
+        if (!settings.getBoolean(key, true)) {
+            keyTV.setVisibility(View.GONE);
+            keyInfo.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // отключила эту функцию, чтобы MainActivity всегда была Main, а остальные - "диалоговыми" окнами
+    }
+
 }
